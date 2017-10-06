21efe5ec70274d82246d3a0d39c3e5df64f25921
Andra Necula <30728373+anecula@users.noreply.github.com>
2017-10-06 15:45:36 +0300

    Update and rename andra.py to web-page-screenshot.py

--- web-page-screenshot.py
+++ web-page-screenshot.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os
+import unittest
+import time
+import datetime
+from datetime import datetime
+
 
 chrome = webdriver.Remote(
           command_executor='http://localhost:4444/wd/hub',
           desired_capabilities=DesiredCapabilities.CHROME)
 firefox = webdriver.Remote(
           command_executor='http://localhost:4444/wd/hub',
-          desired_capabilities=DesiredCapabilities.FIREFOX) 
+          desired_capabilities=DesiredCapabilities.FIREFOX)
 
 chrome.get('https://www.google.com')
 print(chrome.title)
+timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+name = '{}.png'.format(timestamp)
+chrome.save_screenshot(name)
 
-firefox.get('https://www.google.com')
-print(firefox.title)
 
 chrome.quit()
 firefox.quit()
+
